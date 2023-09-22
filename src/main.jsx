70039d75768b037924a52b755270a729eb07f276
SKodehode <stiankodehode@gmail.com>
2023-09-22 11:29:34 +0200

    Removed one file not in use. Added comments to the remaining documents.

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,13 @@ import "./styles/main.css"
 import AppRouter from './pages/Router'
 import ScrollToTop from './components/ScrollToTop'
 
+/* This is the main.jsx file which hosts the initial structure of the page. 
+*  Creating a root container as the main parent container.
+*  Wrapping the child components in strict mode as well as in BrowserRouter imported from react router. 
+*  Lastly there's the AppRouter and the ScrollToTop component which positions the viewer at the top of the page on each page they visit. 
+*  With react router this is not a default feature and without it the users might experience visual annoyances like being at the bottom of a new page.
+*  The AppRouter is our Route component and where the site's pages are set up.
+*/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
